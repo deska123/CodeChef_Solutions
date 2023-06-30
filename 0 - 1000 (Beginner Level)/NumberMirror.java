f7aec9898a4ad8e2c9f7820f1da1845b0d4a75ac
Dennis P Kamah <11691239+deska123@users.noreply.github.com>
2023-06-30 09:57:26 +0700

    Create NumberMirror.java

---
+++ 0 - 1000 (Beginner Level)/NumberMirror.java
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/START01
+
+/* package codechef; // don't place package name! */
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(reader.readLine());
+        System.out.println(t);
+	}
+}  
